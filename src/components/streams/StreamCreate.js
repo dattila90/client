7eeb9c2e40fb8c10262afedb033a3dac9277a78c
dattila90 <demeterattila90@gmail.com>
2020-01-15 21:45:16 +0100

    getting forms with redux-form

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import { Fields, reduxForm, Field } from 'redux-form';
 
-const StreamCreate = () => {
-    return <div>StreamCreate</div>;
+class StreamCreate extends React.Component {
+
+    renderInput(formProps) {
+        return (
+            <input 
+                onChange={formProps.input.onChange} 
+                value={formProps.input.value} 
+            />
+        );
+    }
+
+    render() {
+        console.log("StreamCreate render", this.props);
+        return (
+            <form>
+                <Field name="title" component={this.renderInput} />
+                <Field name="description" component={this.renderInput} />
+            </form>
+        );
+    }
 };
 
-export default StreamCreate;
+export default reduxForm({
+    form: 'streamCreate'
+})(StreamCreate);
