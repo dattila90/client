20929a0c37bc05a0c9eb85a23ff2119ccf38aaa2
dattila90 <demeterattila90@gmail.com>
2020-01-20 22:59:12 +0100

    getting started with rest api

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import { createStream } from '../../actions';
 
 class StreamCreate extends React.Component {
 
@@ -28,8 +30,9 @@ class StreamCreate extends React.Component {
         );
     }
 
-    onSubmit(formValues) {
+    onSubmit = formValues => {
         console.log("onSubmit", formValues);
+        this.props.createStream(formValues);
     }
 
     render() {
@@ -68,7 +71,9 @@ const validate = (formValues) => {
     return errors;
 }
 
-export default reduxForm({
+const formWrapped = reduxForm({
     form: 'streamCreate',
     validate
 })(StreamCreate);
+
+export default connect(null, {createStream})(formWrapped);
